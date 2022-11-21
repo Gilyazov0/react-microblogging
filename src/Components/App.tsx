38322818304463a9aa7538646c77b6de0a603064
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-11-21 21:06:56 +0200

    profile page layout

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Home from "./Home";
 import Profile from "./Profile";
 import { useState } from "react";
+import "./style/App.css";
 
 const App: React.FC = () => {
   const [page, setPage] = useState<"Home" | "Profile">("Profile");
 
   return (
-    <div>
+    <div className="app">
       {page === "Home" && <Home />} {page === "Profile" && <Profile />}
     </div>
   );
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./style/Profile.css";
-
+import { Button } from "react-bootstrap";
 const Profile: React.FC = () => {
-  return <div className="profile"> Profile page</div>;
+  return (
+    <div className="profile">
+      <div className="title">Profile</div>
+      <label htmlFor="name">User name</label>
+      <input type={"text"} name="name" />
+      <Button variant="primary" type="submit">
+        Save
+      </Button>
+    </div>
+  );
 };
 
 export default Profile;
