39c92e40dd3aa434aa92dd01a0fad5b89da70cf0
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-11-23 10:44:09 +0200

    fb deploynemt added

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,25 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
   </React.StrictMode>
 );
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTcRKH3Z0bdupcM_qBVGjiDcrz3GKu-mQ",
+//   authDomain: "micro-blogging--app.firebaseapp.com",
+//   projectId: "micro-blogging--app",
+//   storageBucket: "micro-blogging--app.appspot.com",
+//   messagingSenderId: "224728624560",
+//   appId: "1:224728624560:web:9c96fdbe87b3b7d82d7a75",
+//   measurementId: "G-ZL712TRRHW",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
