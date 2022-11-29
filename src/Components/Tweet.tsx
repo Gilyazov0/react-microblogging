be003cc44a8aea4f91ff008a7bedbbdc2a9d7553
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-11-29 09:15:29 +0200

    date formating

--- src/Components/Tweet.tsx
+++ src/Components/Tweet.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { TweetProps } from "../Types/TweetProps";
 import "./style/Tweet.css";
 
@@ -7,7 +8,9 @@ const Tweet: React.FC<TweetProps> = (props: TweetProps) => {
       <div className="d-flex">
         <div className="text-secondary">{props.userName}</div>
         <div className="flex-grow-1"></div>
-        <div className="text-secondary">{props.date}</div>
+        <div className="text-secondary">
+          {moment(props.date).format("MMM Mo HH:mm A")}
+        </div>
       </div>
       <div>{props.content}</div>
     </div>
--- src/Components/TweetsList.tsx
+++ src/Components/TweetsList.tsx
@@ -4,6 +4,7 @@ import { TweetProps } from "../Types/TweetProps";
 import { TweetsContext } from "./Home";
 import Loading from "./Loading";
 import Tweet from "./Tweet";
+import moment from "moment";
 
 const TweetList: React.FC<{ getTweets: Function; hasMore: boolean }> = (
   props
