055fc75ae8e00993be648d38cafe954c79f99605
Sergey Gilyazov <s.gilyazov@gmail.com>
2022-11-30 14:44:04 +0200

    seachAt styling

--- src/Components/NavBar/SearchAt.tsx
+++ src/Components/NavBar/SearchAt.tsx
@@ -1,14 +1,18 @@
 import Dropdown from "react-bootstrap/Dropdown";
 import { SearchAtType } from "../../SearchTypes";
-
+import "../style/SearchAt.css";
 const SearchAt: React.FC<{
   setSearchAt: Function;
   searchAt: SearchAtType;
 }> = ({ setSearchAt, searchAt }) => {
   return (
     <Dropdown>
-      <Dropdown.Toggle variant={"secondary"} id="dropdown-basic">
-        {searchAt}
+      <Dropdown.Toggle
+        variant={"secondary"}
+        id="dropdown-basic"
+        className="searchAt-button"
+      >
+        <span>{searchAt} </span>
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
